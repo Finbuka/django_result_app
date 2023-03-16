3a9d619429a887526c86d5eaca1d2b688584955f
Finbuka <ebukajoe100@gmail.com>
2023-03-16 04:43:55 +0100

    second

--- economics_result/settings.py
+++ economics_result/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 from os import getenv
 import dj_database_url
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
