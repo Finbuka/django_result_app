a7e19018bec214578876f53d64e731b3c4b47abf
Finbuka <ebukajoe100@gmail.com>
2023-03-16 12:56:56 +0100

    division by zero error

--- students/templatetags/tags_extra.py
+++ students/templatetags/tags_extra.py
@@ -4,7 +4,11 @@ register = template.Library()
 
 @register.filter
 def divide(value, arg):
-    return value/arg
+    try:
+        var_divided = value/arg
+    except Exception as e:
+        var_divided = 0
+    return var_divided
 
 @register.simple_tag
 def define(value=None):
